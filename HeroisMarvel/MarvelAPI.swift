eecbe2680b749060186a38a0630d34baa81ee64a
Eduardo Frederico <eduardo.frederico@icloud.com>
2025-02-06 00:16:13 -0300

    Consumindo a API da Marvel com Alamofire.

---
+++ HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,8 @@
+//
+//  marvel.swift
+//  HeroisMarvel
+//
+//  Created by Eduardo Frederico on 05/02/25.
+//  Copyright © 2025 Eric Brito. All rights reserved.
+//
+
---
+++ HeroisMarvel/MarvelClasses.swift
@@ -0,0 +1,8 @@
+//
+//  MarvelClasses.swift
+//  
+//
+//  Created by Eduardo Frederico on 05/02/25.
+//
+
+import Foundation
